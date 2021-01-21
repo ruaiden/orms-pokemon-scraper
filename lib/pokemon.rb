c13fe1257f6d87968c6560a8eecd7c62989c767a
Aiden <aiden.marques94@gmail.com>
2021-01-20 22:05:52 -0500

    commit tree 8c52f89ad5386e1845aec67a30482dfb3de507fc

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,19 +1,44 @@
 
 
 class Pokemon
-    attr_accessor :id, :name, :type, :db
+    attr_accessor :name, :type, :db
+    attr_reader :id
   
-    def self.save(name, type, db)
-      db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
-    end
-  
-    def self.find(id_num, db)
-      pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-      Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
-    end
-  
+
     def initialize(id:, name:, type:, db:)
-      @id, @name, @type, @db = id, name, type, db
+        @id, @name, @type, @db = id, name, type, db
+      end
+
+
+    def self.save(name, type, db)
+        
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0] 
+    end 
+
+    def self.new_from_db(row)
+        pokemon = Pokemon.new(id: row[0],name: row[1],type: row[2],db: row[3])
+        pokemon    
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = ?
+            LIMIT 1
+        SQL
+
+        db.execute(sql, id).map {|row| self.new_from_db(row)}.first
     end
   
+    
+  
+   
+
   end
